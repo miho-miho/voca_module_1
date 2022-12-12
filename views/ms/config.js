197ffded7b448f67c9396cc31460237b81c7997c
Miho Koinuma <miho.021431.music@gmail.com>
2022-12-12 11:52:07 +0900

    各言語のファイル作成＋config.js

---
+++ views/ms/config.js
@@ -0,0 +1,19 @@
+//pl：config.js
+lang_info = {
+  lang : "",
+  lang_jp : "マレーシア語",
+  message1 : "",
+  message2 : "",
+  vmod_ms1 : [""],
+  vmod_ms1_url : { "" : "" },
+};
+
+sidebar_setting = {
+  sub_menu_snd : "",
+  sub_menu_talk : "",
+  sub_menu_gram : "",
+  sub_menu_vocab : "e",
+}
+
+exports.lang_info = lang_info
+exports.sidebar_setting = sidebar_setting
