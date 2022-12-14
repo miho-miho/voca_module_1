e7a8b71b8a2687cecdc5ca28be4df38e60d5d4d4
Miho Koinuma <miho.021431.music@gmail.com>
2022-12-14 11:53:50 +0900

    Update config.js

--- views/fr/config.js
+++ views/fr/config.js
@@ -4,7 +4,7 @@ lang_info = {
   lang_jp : "フランス語",
   message1 : "",
   message2 : "",
-  vmod_ms1 : ["Dubois Jean (dir.), (1978) Dictionnaire du français langue étrangère, niveau 1, Larousse.", Miquel Claire (2002) Vocabulaire progressif du français, niveau débutant, CLE International.],
+  vmod_ms1 : ["Dubois Jean (dir.), (1978) Dictionnaire du français langue étrangère, niveau 1, Larousse.", "Miquel Claire (2002) Vocabulaire progressif du français, niveau débutant, CLE International."],
   vmod_ms1_url : { "" : "" },
 };
 
