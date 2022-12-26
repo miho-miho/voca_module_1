e2ac2ce127493cd2e221e3045cb3c3fb2658f85b
Miho Koinuma <miho.021431.music@gmail.com>
2022-12-26 11:20:10 +0900

    Update app1.js

--- app1.js
+++ app1.js
@@ -107,19 +107,28 @@ app.get('/:lang/v/table', (req, res) => {
     make_vObj : make_vObj
   });
 });
+
+var search_result_list = []
 //分類表_結果リスト
-app.get('/:lang/v/table/v_search_list=:chuno', (req, res)=> {
+app.get('/:lang/v/t_search_list=:chuno', (req, res)=> {
   let lang = req.params.lang
   let currentWorkingDirectory = process.cwd();
   let pathToLnag = currentWorkingDirectory+'/views/'+lang
   var info = require(pathToLnag + "/config")
+  var category;
   let chuno = req.params.chuno
     Object.keys(word_obj_all[lang]).forEach(function(key) {
       if(word_obj_all[lang][key]["chuno"] === chuno){
-        console.log(word_obj_all[lang][key]);
+        category = word_obj_all[lang][key]["chukomoku"]
+        search_result_list.push(word_obj_all[lang][key]);
       }
     });
-  res.send(req.params.chuno)
+  res.render(pathToLnag + '/vmod/v_search_result.ejs', {
+    lg : lang,
+    lang_jp : info.lang_info.lang_jp,
+    search_result_list: search_result_list,
+    category: category
+  });
 });
 /*
 //検索(日・ポ)
@@ -142,7 +151,6 @@ app.get('/smod', (req,res) => {
     category_k: category_k1.concat(category_k2)
     });
 });
-*/
 //詳細_分類表
 app.post('/:lang/v/detail', (req, res) => {
   let lang = req.params.lang
@@ -157,6 +165,7 @@ app.post('/:lang/v/detail', (req, res) => {
     pl_word: req.body.pl_word
   });
 });
+*/
 //詳細_基礎
 app.post('/:lang/v/detail_kiso', (req, res) => {
   let lang = req.params.lang
