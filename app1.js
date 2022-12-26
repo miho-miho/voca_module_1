4389dc816f033ba910617b5df705fc0110a9efab
Miho Koinuma <miho.021431.music@gmail.com>
2022-12-26 15:46:31 +0900

    Update app1.js

--- app1.js
+++ app1.js
@@ -27,23 +27,6 @@ var bunrui_so = json_bunrui["so"]
 app.get('/', (req, res) => {
   res.render('index1.0.ejs');
 });
-/*
-//言語トップ
-app.get("/mt/:lang", (req, res) => {
-  let lang = req.params.lang
-  let currentWorkingDirectory = process.cwd();
-  let pathToLnag = currentWorkingDirectory+'/views/'+lang
-  var info = require(pathToLnag + "/config")
-  res.render(pathToLnag + '/top/top.ejs', {
-    lg : lang,
-    lang : info.lang_info.lang,
-    lang_jp : info.lang_info.lang_jp,
-    message1 : info.lang_info.message1,
-    message2 : info.lang_info.message2,
-    sidebar_setting : info.sidebar_setting
-  });
-});
-*/
 let currentWorkingDirectory = process.cwd();
 //語彙モジュールトップ
 app.get('/:lang/v/', (req, res) => {
@@ -132,42 +115,6 @@ app.get('/:lang/v/t_search_list=:chuno', (req, res)=> {
   });
   search_result_list = []
 });
-/*
-//検索(日・ポ)
-app.get('/vmod_search', (req, res) => {
-  res.render(__dirname + '/views/ polish/vmod_search0.ejs', {
-    vocab_obj_k1: vocab_obj_k1,
-    vocab_obj_k2: vocab_obj_k2,
-    vocab_obj_b: vocab_obj_b_all,
-    category: category_b1.concat(category_b2, category_b3)
-  });
-});
-var word_obj_b_all = Object.assign(word_obj_b1, word_obj_b2, word_obj_b3)
-//例文検索
-app.get('/smod', (req,res) => {
-  res.render(__dirname + '/views/ polish/smod1.ejs', {
-    data1: word_obj_b_all,
-    data2: word_obj_k1,
-    data3: word_obj_k2,
-    category_b: category_b1.concat(category_b2, category_b3),
-    category_k: category_k1.concat(category_k2)
-    });
-});
-//詳細_分類表
-app.post('/:lang/v/detail', (req, res) => {
-  let lang = req.params.lang
-  let currentWorkingDirectory = process.cwd();
-  let pathToLnag = currentWorkingDirectory+'/views/'+lang
-  var info = require(pathToLnag + "/config")
-  res.render(pathToLnag + '/vmod/vmod_search_detail.ejs', {
-    lg : lang,
-    lang_jp : info.lang_info.lang_jp,
-    data1: word_obj_b_all,
-    category: req.body.category,
-    pl_word: req.body.pl_word
-  });
-});
-*/
 //詳細_基礎
 app.post('/:lang/v/c_detail=:category', (req, res) => {
   let lang = req.params.lang
