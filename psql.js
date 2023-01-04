7991ac995a0511cea12b8ba09658eb37a7410137
Miho Koinuma <miho.021431.music@gmail.com>
2023-01-04 16:27:57 +0900

    Update psql.js

--- psql.js
+++ psql.js
@@ -23,7 +23,7 @@ client.connect((err) => {
 });
 
 const query = {
-  text: "SELECT t_usage.usage_id,t_usage.word_id,rui,chukoumoku_no,chukoumoku,basic,midasi FROM t_usage_classified_rel JOIN t_usage ON t_usage_classified_rel.usage_id=t_usage.usage_id JOIN t_word ON t_usage.word_id=t_word.id WHERE t_usage_classified_rel.chukoumoku_no='4.30'"
+  text: "SELECT t_usage.usage_id,t_usage.word_id,rui,chukoumoku_no,chukoumoku,basic,midasi FROM t_usage_classified_rel JOIN t_usage ON t_usage_classified_rel.usage_id=t_usage.usage_id JOIN t_word ON t_usage.word_id=t_word.id WHERE t_usage_classified_rel.chukoumoku_no='1.10'"
 };
 client
   .query(query)
