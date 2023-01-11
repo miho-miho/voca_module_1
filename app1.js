d46dccc390727e84c7442ebe93267a1c9b3f44f5
Miho Koinuma <miho.021431.music@gmail.com>
2023-01-11 12:41:27 +0900

    Update app1.js

--- app1.js
+++ app1.js
@@ -131,7 +131,6 @@ app.get('/:lang/v/t_search_list=:chuno', (req, res)=> {
   var info = require(pathToLnag + "/config")
   var category;
   let chuno = String(req.params.chuno)
-  console.log(chuno);
   var client = new Client({
     user: info.db_info.user,
     host: info.db_info.host,
@@ -140,7 +139,10 @@ app.get('/:lang/v/t_search_list=:chuno', (req, res)=> {
     port: 5432
   })
   client.connect();
-  const query = "SELECT t_usage.usage_id,t_usage.word_id,chukoumoku_no,basic,midasi FROM t_usage_classified_rel JOIN t_usage ON t_usage_classified_rel.usage_id=t_usage.usage_id JOIN t_word ON t_usage.word_id=t_word.id WHERE t_usage_classified_rel.chukoumoku_no = ?";
+  const query = {
+    text: "SELECT t_usage.usage_id,t_usage.word_id,chukoumoku_no,basic,midasi FROM t_usage_classified_rel JOIN t_usage ON t_usage_classified_rel.usage_id=t_usage.usage_id JOIN t_word ON t_usage.word_id=t_word.id WHERE t_usage_classified_rel.chukoumoku_no=$1",
+    values: [chuno]
+  };
   client.query(query, [chuno], (err, result) => {
     //if (err) throw err;
     console.log(result);
