38f67d6947530b7d7e34aa30a19c69cc64ea1589
miho-miho <miho.021431.music@gmail.com>
2023-01-16 16:35:10 +0900

    Update app1.js

--- app1.js
+++ app1.js
@@ -187,7 +187,7 @@ app.post('/:lang/v/c_detail=:category', (req, res) => {
       console.log(item);
       tObj.midasi = item.basic
       tObj.instances = []
-      tObj.instances.push("usage":item.explanation)
+      tObj.instances["usage"] = item.explanation
       tObj.instances.push({"insts":[((({ basic, usage_id, explanation, ...rest }) => rest)(item))]})
       if (tObj.instances.usage === item.explanation) {
         tObj.instances.insts.push(((({ basic, usage_id, explanation, ...rest }) => rest)(item)))
