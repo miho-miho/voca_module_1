4539601a5b680884fce90508b177e05b64bebc13
miho-miho <miho.021431.music@gmail.com>
2023-01-18 14:00:25 +0900

    Update app1.js

--- app1.js
+++ app1.js
@@ -203,7 +203,6 @@ app.post('/:lang/v/c_detail=:category', (req, res) => {
       var sameMidasi = insts.find((element) => element.midasi === midasi)
       if (sameMidasi) {
         sameMidasi.inst.push({"usage":ex, "reibun":[li]})
-        console.log(li);
       } else {
         inst.push({"usage":ex, "reibun":[li]})
         var result = {"midasi":midasi, "inst":inst}
