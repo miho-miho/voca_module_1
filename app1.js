6cdf93ac832a5a2e982a4bf66178922da241f945
miho-miho <miho.021431.music@gmail.com>
2023-01-18 12:45:21 +0900

    Update app1.js

--- app1.js
+++ app1.js
@@ -156,8 +156,8 @@ app.post('/:lang/v/c_detail=:category', (req, res) => {
   let lang = req.params.lang
   let category = req.params.category
   let targetSceneId = req.body.targetSceneId
-  let targetWordIds = [req.body.targetWordIds]
-  console.log(req.body);
+  let targetWordIds = req.body.targetWordIds.split(',')
+  console.log(targetWordIds);
   let currentWorkingDirectory = process.cwd();
   let pathToLnag = currentWorkingDirectory+'/views/'+lang
   var info = require(pathToLnag + "/config")
