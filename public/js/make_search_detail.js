c72a36151a110d6a5e051627c932f1b0fbf0ab7f
miho-miho <miho.021431.music@gmail.com>
2023-01-25 12:52:50 +0900

    Update make_search_detail.js

--- public/js/make_search_detail.js
+++ public/js/make_search_detail.js
@@ -154,13 +154,14 @@ exports.getDmodlink = function(xml_file_name, xpath, lang){
     var stid = "st_"+Number(line)+"_"+Number(sentence);
     var pmodpage = "";
     fetch(htmlfile).then((resp) => resp.text()).then(function(data) {
-    var lines = data.split('\n');
-    for (var i = 0; i < lines.length; i++) {
-      if (lines[i].indexOf("_timeCounterStArray") !== -1) {
-        if (lines[i].indexOf(`["${stid}"]`) !== -1) {
-          pmodpage = lines[i]        // _timeCounterStArray["st_0_0"] = new Array("2.5", "4.98");
+      var lines = data.split('\n');
+      for (var i = 0; i < lines.length; i++) {
+        if (lines[i].indexOf("_timeCounterStArray") !== -1) {
+          if (lines[i].indexOf(`["${stid}"]`) !== -1) {
+            pmodpage = lines[i]        // _timeCounterStArray["st_0_0"] = new Array("2.5", "4.98");
+          }
         }
-      }
+      });
     });
     var matches = pmodpage.match(/new Array\(\"([\d|\.]+)\", \"([\d|\.]+)\"\);/)
     if (matches === null) {
