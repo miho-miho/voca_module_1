2cb9231d8120a1e020b68be8e423df25966778ff
miho-miho <miho.021431.music@gmail.com>
2023-01-25 13:22:16 +0900

    Update make_search_detail.js

--- public/js/make_search_detail.js
+++ public/js/make_search_detail.js
@@ -92,7 +92,6 @@ exports.getGmodLink = function(xml_file_name, xpath, lang){
 }
 //getDmodSoundFile
 exports.getDmodSoundFile = function(xml_file_name, xpath, lang){
-  console.log(xml_file_name, xpath);
   var dmod_funcId = "";
   var matches = xml_file_name.match(/^(.*)(\d{2})\.xml/)
   if (matches != null) {
@@ -115,7 +114,6 @@ exports.getDmodSoundFile = function(xml_file_name, xpath, lang){
     sentence = matches[2] -1;
   }
   var stid = "st_"+Number(line)+"_"+Number(sentence);
-  console.log(stid);
   var pmodpage = "";
   if (htmlfile === "" | htmlfile === null) {
     return "";
@@ -130,30 +128,30 @@ exports.getDmodSoundFile = function(xml_file_name, xpath, lang){
           }
         }
       }
-    });
-  }
-  var matches = pmodpage.match(/new Array\(\"([\d|\.]+)\", \"([\d|\.]+)\"\);/)
-  if (matches === null) {
-    return "";
-  }
-  var start = matches[1];
-  var stop = matches[2];
-  var matches = htmlfile.match(/.*(\d{2})\.html/)
-  if (matches === null) {
-    return "";
-  }
-  var dmodsound = `../../../mt/${lang}/dmod/class/movie/${lang}_ja${matches[1]}.mp4`
-  console.log(dmodsound);
-  var ret = `
-    <!-- ${dmodsound} -->
-    <span  class='dmodsound' onclick="playDmodSound('${dmodsound}', '${start}', '${stop}')">
-    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-play-circle" viewBox="0 0 16 16">
-      <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
-      <path d="M6.271 5.055a.5.5 0 0 1 .52.038l3.5 2.5a.5.5 0 0 1 0 .814l-3.5 2.5A.5.5 0 0 1 6 10.5v-5a.5.5 0 0 1 .271-.445z"/>
-      </svg>
-    </span>
-    `
-    return ret
+    }).then(function(pmodpage){
+      matches = pmodpage.match(/new Array\(\"([\d|\.]+)\", \"([\d|\.]+)\"\);/)
+      if (matches === null) {
+        return "";
+      }
+      var start = matches[1];
+      var stop = matches[2];
+      var matches = htmlfile.match(/.*(\d{2})\.html/)
+      if (matches === null) {
+        return "";
+      }
+      var dmodsound = `../../../mt/${lang}/dmod/class/movie/${lang}_ja${matches[1]}.mp4`
+      var ret = `
+        <!-- ${dmodsound} -->
+        <span  class='dmodsound' onclick="playDmodSound('${dmodsound}', '${start}', '${stop}')">
+        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-play-circle" viewBox="0 0 16 16">
+          <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
+          <path d="M6.271 5.055a.5.5 0 0 1 .52.038l3.5 2.5a.5.5 0 0 1 0 .814l-3.5 2.5A.5.5 0 0 1 6 10.5v-5a.5.5 0 0 1 .271-.445z"/>
+          </svg>
+        </span>
+        `
+        return ret
+      });
+    }
   }
 //getDmodlink
 exports.getDmodlink = function(xml_file_name, xpath, lang){
