fb95b9a07ec1d39c96371d365fd6fc4d011bf0c2
miho-miho <miho.021431.music@gmail.com>
2023-01-25 12:25:32 +0900

    Update app1.js

--- app1.js
+++ app1.js
@@ -195,7 +195,8 @@ app.post('/:lang/v/c_detail=:category', (req, res) => {
       for (var e of item) {
         var ex = e.explanation
         e = (({ basic, usage_id, explanation, ...rest }) => rest)(e)
-        console.log(e);
+        var link = mkDetail.getGmodLink(e.xml_file_name, e.xpath, lang)
+        console.log(link);
         li.push(e)
       }
       var midasi = item[0].basic
