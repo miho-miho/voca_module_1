0f5aa3ddffef52e34a23d2db8a1cfc65ec078ceb
miho-miho <miho.021431.music@gmail.com>
2023-01-25 15:38:58 +0900

    Update make_search_detail.js

--- public/js/make_search_detail.js
+++ public/js/make_search_detail.js
@@ -257,12 +257,12 @@ exports. makeModLink = function(module_id, xml_file_name, xpath, lang){
       var dmodsound = `../../../mt/${lang}/dmod/class/movie/${lang}_ja${matches[1]}.mp4`
       var ret = `
         <!-- ${dmodsound} -->
-        <span  class='dmodsound' onclick="playDmodSound('${dmodsound}', '${start}', '${stop}')">
+        <button  class='dmodsound' onclick="playDmodSound('${dmodsound}', '${start}', '${stop}')">
         <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-play-circle" viewBox="0 0 16 16">
           <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
           <path d="M6.271 5.055a.5.5 0 0 1 .52.038l3.5 2.5a.5.5 0 0 1 0 .814l-3.5 2.5A.5.5 0 0 1 6 10.5v-5a.5.5 0 0 1 .271-.445z"/>
           </svg>
-        </span>
+        </button>
         `
         return ret
       }
