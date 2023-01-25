30217a63f3df0d0b2b2a4021ec9ed7c3eca3b628
miho-miho <miho.021431.music@gmail.com>
2023-01-25 12:36:14 +0900

    Update make_search_detail.js

--- public/js/make_search_detail.js
+++ public/js/make_search_detail.js
@@ -6,6 +6,7 @@ function file_get_contents(filename) {
 }
 //-----------------------------//
 //getGmodSoundFile
+/*
 exports.getGmodSoundFile = function(xml_file_name, xpath){
   if (xml_file_name == null || xml_file_name == "" || xpath == null || xpath == "")  {
     return "";
@@ -40,6 +41,7 @@ exports.getGmodSoundFile = function(xml_file_name, xpath){
     return ret
   }
 }
+*/
 //getGmodLink
 exports.getGmodLink = function(xml_file_name, xpath, lang){
   function　getGmodSoundFile(xml_file_name, xpath){
@@ -98,6 +100,11 @@ exports.getDmodSoundFile = function(htmlfile, xpath){
   }
   var stid = "st_"+Number(line)+"_"+Number(sentence);
   var pmodpage = "";
+  function file_get_contents(filename) {
+      fetch(filename).then((resp) => resp.text()).then(function(data) {
+          return data;
+      });
+  }
   var data = file_get_contents(htmlfile)
   var lines = data.split('\n');
   for (var i = 0; i < lines.length; i++) {
@@ -131,13 +138,59 @@ exports.getDmodSoundFile = function(htmlfile, xpath){
     return ret
   }
 //getDmodlink
-exports.getDmodlink = function(xml_file_name, xpath){
+exports.getDmodlink = function(xml_file_name, xpath, lang){
+  function getDmodSoundFile(htmlfile, xpath){
+    var line =  "";
+    var sentence = "";
+    var matches = xpath.match(/line\[(\d+)\]\/sentence\[(\d+)\]/)
+    if (matches != null) {
+      line = matches[1] - 1;
+      sentence = matches[2] -1;
+    }
+    var stid = "st_"+Number(line)+"_"+Number(sentence);
+    var pmodpage = "";
+    function file_get_contents(filename) {
+        fetch(filename).then((resp) => resp.text()).then(function(data) {
+            return data;
+        });
+    }
+    var data = file_get_contents(htmlfile)
+    var lines = data.split('\n');
+    for (var i = 0; i < lines.length; i++) {
+      if (lines[i].indexOf("_timeCounterStArray") !== -1) {
+        if (lines[i].indexOf(`["${stid}"]`) !== -1) {
+          pmodpage = lines[i]        // _timeCounterStArray["st_0_0"] = new Array("2.5", "4.98");
+        }
+      }
+    }
+    var matches = pmodpage.match(/new Array\(\"([\d|\.]+)\", \"([\d|\.]+)\"\);/)
+    if (matches === null) {
+      return "";
+    }
+    var start = matches[1];
+    var stop = matches[2];
+    var lang = "<%- lg %>"
+    var matches = htmlfile.match(/.*(\d{2})\.html/)
+    if (matches === null) {
+      return "";
+    }
+    var dmodsound = `../../../mt/${lang}/dmod/class/movie/${lang}_ja${matches[1]}.mp4`
+    var ret = `
+      <!-- ${dmodsound} -->
+      <span  class='dmodsound' onclick="playDmodSound('${dmodsound}', '${start}', '${stop}')">
+      <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-play-circle" viewBox="0 0 16 16">
+        <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
+        <path d="M6.271 5.055a.5.5 0 0 1 .52.038l3.5 2.5a.5.5 0 0 1 0 .814l-3.5 2.5A.5.5 0 0 1 6 10.5v-5a.5.5 0 0 1 .271-.445z"/>
+        </svg>
+      </span>
+      `
+      return ret
+    }
   var dmod_funcId = "";
   var matches = xml_file_name.match(/^(.*)(\d{2})\.xml/)
   if (matches != null) {
     dmod_funcId = matches[2]; // ex. 15
   }
-  var lang = "<%- lg %>"
   var lang_matches = lang.match(/ja_([a-z][a-z])/)
   // 英語ページ 英語はHTMLファイルの名前がfunc_01.htmlとstory_01.htmlの２種ある
   if (lang === "en") {
