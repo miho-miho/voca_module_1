eb90f5a7cfa7d9955b74d7bb890014c21826bc9c
miho-miho <miho.021431.music@gmail.com>
2023-01-25 16:37:39 +0900

    Update app1.js

--- app1.js
+++ app1.js
@@ -262,7 +262,6 @@ app.post('/:lang/v/t_search_detail=:chuno', (req, res) => {
       for (var e of item) {
         var ex = e.explanation
         e = (({ basic, usage_id, explanation, ...rest }) => rest)(e)
-        console.log(e.module_id, e.xml_file_name, e.xpath, lang);
         var link = mkDetail.makeModLink(e.module_id, e.xml_file_name, e.xpath, lang)
         e.link = link
         li.push(e)
