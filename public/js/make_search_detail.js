1764dc7150fc513219913c6046c8c99aeff54017
miho-miho <miho.021431.music@gmail.com>
2023-02-06 11:07:01 +0900

    Update make_search_detail.js

--- public/js/make_search_detail.js
+++ public/js/make_search_detail.js
@@ -348,6 +348,7 @@ exports.makeModLink = function(module_id, xml_file_name, xpath, lang){
 exports.makeInstSound = function(instid, lang){
   var audioBlock = "";
   var instsoundfile = `../../../mt/${lang}/vmod/sound/inst/inst_${instid}.mp3`;
+  console.log(instsoundfile);
   if (fs.existsSync(instsoundfile)) {
     audioBlock = `
       <audio id='instSound_${instid}'>
