8e290725247649d72a65af8b25e7c124910c4c30
miho-miho <miho.021431.music@gmail.com>
2023-02-08 12:29:31 +0900

    Update app1.js

--- app1.js
+++ app1.js
@@ -515,7 +515,7 @@ app.get('/:lang/v/v_search_list-str=:char&st=:st', (req, res) => {
       k["senses"] = s_list
       r_list.push(k);
     }
-    targetChar = targetChar.replace("%", "~")
+    targetChar = targetChar.replace(/%/g, "~")
     res.render(pathToLnag + '/vmod/v_search_result_list.ejs', {
       lg : lang,
       lang_jp : info.lang_info.lang_jp,
