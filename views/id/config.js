59a32b95552f0e6b959944c03672f024473ec6e3
miho-miho <miho.021431.music@gmail.com>
2023-02-15 12:32:00 +0900

    Update config.js

--- views/id/config.js
+++ views/id/config.js
@@ -1,19 +1,19 @@
 //pl：config.js
 lang_info = {
-  lang : "",
+  lang : "Polski",
   lang_jp : "インドネシア語",
-  message1 : "",
-  message2 : "",
-  vmod_ms1 : [""],
+  message1 : "人口約２億人を抱えるインドネシア共和国の国語です。国内では地域によってジャワ語やスンダ語をはじめとする数百に及ぶ地方語が使われていますが、インドネシア語は国語・公用語としてインドネシア全体に行き渡っています。２００２年に独立した東ティモールでは、実用語に定められています。",
+  message2 : "ここでは、標準とされているインドネシア語を基本として学びます。会話が標準的な語法とは若干異なっていたり、発音がそれぞれの地方語の影響を受けていることもありますが、ここで学ぶインドネシア語は教育やテレビでも用いられており、インドネシア全体で使えるものです。",
+  vmod_ms1 : ["Tim Penyusun Kamus Pusat Bahasa (2001) Kamus Besar Bahasa Indonesia (Edisi III). Jakarta: Balai Pustaka.", "Alan M. Stevens and A. Ed. Schmidgall-Tellings (2004) A Comprehensive Indonesian-English Dictionary. Athens: Ohio University Press.", "佐々木重次編 (2005) 『最新インドネシア語小辞典』第1.2版．Grup Sanggar.", "左藤正範編 (2005) 『インドネシア語分類辞典』．大学書林．"],
   vmod_ms1_url : { "" : "" },
 };
 
-sidebar_setting = {
-  sub_menu_snd : "",
-  sub_menu_talk : "",
-  sub_menu_gram : "",
-  sub_menu_vocab : "",
+db_info = {
+  user : "id",
+  host : "localhost",
+  database : "vmod_id",
+  password : "banana",
 }
 
 exports.lang_info = lang_info
-exports.sidebar_setting = sidebar_setting
+exports.db_info = db_info
