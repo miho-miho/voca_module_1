9415ffd4e0d8ef24b849503f23c88deb0d197705
miho-miho <miho.021431.music@gmail.com>
2023-02-15 15:03:14 +0900

    Update config.js

--- views/en/config.js
+++ views/en/config.js
@@ -8,12 +8,12 @@ lang_info = {
   vmod_ms1_url : { "" : "" },
 };
 
-db_info = {
-  user : "en",
-  host : "localhost",
-  database : "vmod_en",
-  password : "roastbeef",
+sidebar_setting = {
+  sub_menu_snd : "",
+  sub_menu_talk : "",
+  sub_menu_gram : "",
+  sub_menu_vocab : "",
 }
 
 exports.lang_info = lang_info
-exports.db_info = db_info
+exports.sidebar_setting = sidebar_setting
