2cb5a4a050b1ca3ca146bdcb3e15670abbe66fa8
miho-miho <miho.021431.music@gmail.com>
2023-02-15 13:22:00 +0900

    Update make_search_detail.js

--- public/js/make_search_detail.js
+++ public/js/make_search_detail.js
@@ -208,7 +208,7 @@ exports.getDmodlink = function(xml_file_name, xpath, lang){
   return link
 }
 */
-exports.makeModLink = function(module_id, xml_file_name, xpath, targetlanguage, lang){
+exports.makeModLink = function(module_id, xml_file_name, xpath, web_url, targetlanguage, lang){
   //getDmodlink
   function getDmodlink(xml_file_name, xpath, lang){
     function getDmodSoundFile(htmlfile, xpath, lang){
