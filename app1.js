17b043db7e6b8bd480ec7929509c499c5255fc77
miho-miho <miho.021431.music@gmail.com>
2023-02-15 11:14:01 +0900

    Update app1.js

--- app1.js
+++ app1.js
@@ -239,7 +239,7 @@ app.post('/:lang/v/search_detail=:tag', (req, res) => {
   let pathToLnag = currentWorkingDirectory+'/views/'+lang
   var info = require(pathToLnag + "/config")
   let tag = req.params.tag;
-  console.log(tag);
+  console.log(req.body.category);
   var client = new Client({
     user: info.db_info.user,
     host: info.db_info.host,
