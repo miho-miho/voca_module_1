a965f68c0b7eccfbcdbc8d34a36ddcd196e1c63d
miho-miho <miho.021431.music@gmail.com>
2023-02-15 13:35:45 +0900

    Update make_search_detail.js

--- public/js/make_search_detail.js
+++ public/js/make_search_detail.js
@@ -358,13 +358,17 @@ exports.makeModLink = function(module_id, xml_file_name, xpath, web_url, targetl
             matches = lines[i].match(/<.*?playItem\('(.*)'\)/);
           }
         }
-        console.log(matches[1]);
-        var pmodsound = `../${pmodPath}/sound/${matches}[1].mp3`;
+        var pmodsound = `../${pmodPath}/sound/${matches[1]}.mp3`;
         var ret = `
         <audio>
-          <source src='$pmodsound' type='audio/mp3'>
+          <source src='${pmodsound}' type='audio/mp3'>
         </audio>
-        <span class='soundLink instSound'>{$this->playIcon}</span>
+        <button type="button" class="btn btn-warnig soundLink instSound">
+              <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-play-circle" viewBox="0 0 16 16">
+                <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
+                <path d="M6.271 5.055a.5.5 0 0 1 .52.038l3.5 2.5a.5.5 0 0 1 0 .814l-3.5 2.5A.5.5 0 0 1 6 10.5v-5a.5.5 0 0 1 .271-.445z"/>
+              </svg>
+            </button>
         `
         return ret;
     }
