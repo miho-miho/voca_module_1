c04f6874435075e853abda2007374fbffe24a75b
miho-miho <miho.021431.music@gmail.com>
2023-02-15 13:30:17 +0900

    Update make_search_detail.js

--- public/js/make_search_detail.js
+++ public/js/make_search_detail.js
@@ -351,7 +351,7 @@ exports.makeModLink = function(module_id, xml_file_name, xpath, web_url, targetl
       } else {
         pmodPath = `../../mt/${lang}/pmod2/${web_url}`
       }
-        var result = fs.readFileSync(htmlfile, 'utf8');
+        var result = fs.readFileSync(pmodPath, 'utf8');
         var lines = result.split('\n');
         for (var i = 0; i < lines.length; i++) {
           console.log(lines[i]);
