ef018385881e8730e5aa1f1cc91d4bb71483e54c
miho-miho <miho.021431.music@gmail.com>
2023-02-15 16:29:37 +0900

    Update app1.js

--- app1.js
+++ app1.js
@@ -193,6 +193,7 @@ app.post('/:lang/v/search_detail=:tag', (req, res) => {
     }
     var insts = []
     instances.forEach((item) => {
+      console.log(item);
       var midasi = item[0].basic
       var li = []
       for (var e of item) {
