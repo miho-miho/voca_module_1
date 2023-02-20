f6d44659d226e9efa10206773a56df56afda1c7d
miho-miho <miho.021431.music@gmail.com>
2023-02-20 15:00:56 +0900

    Update app1.js

--- app1.js
+++ app1.js
@@ -399,7 +399,7 @@ app.post('/:lang/v/s_search_detail-tg=:char', (req, res) => {
         var result = {"midasi":midasi, "midashiaudio":midashiaudio, "inst":inst}
         insts.push(result)
       }
-      if (midasi = "جُمَادَى ٱلثَّانِيَةُ") {
+      if (midasi === "جُمَادَى ٱلثَّانِيَةُ") {
         console.log(li);
       }
     });
