c563eb545d79410fb92792a780dc5ad60062efb3
miho-miho <miho.021431.music@gmail.com>
2023-02-20 12:50:07 +0900

    Update app1.js

--- app1.js
+++ app1.js
@@ -391,6 +391,7 @@ app.post('/:lang/v/s_search_detail-tg=:char', (req, res) => {
       var midashiaudio = mkDetail.makeWordSound(item[0].id, lang)
       var inst = []
       var sameMidasi = insts.find((element) => element.midasi === midasi)
+      console.log(li.length);
       li = li.filter((element, index, self) => self.findIndex(e => e.instid === element.instid ) === index);
       if (sameMidasi) {
         sameMidasi.inst.push({"usage":ex, "reibun":[li]})
