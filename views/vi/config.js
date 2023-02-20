f0d29b8842e030012f4aa9ec1da2b3060b7318dc
miho-miho <miho.021431.music@gmail.com>
2023-02-20 11:32:54 +0900

    Update config.js

--- views/vi/config.js
+++ views/vi/config.js
@@ -4,16 +4,16 @@ lang_info = {
   lang_jp : "ベトナム語",
   message1 : "",
   message2 : "",
-  vmod_ms1 : [""],
+  vmod_ms1 : ["Viện Ngôn Ngữ Học(2004)「Từ Điển Tiếng Việt」（Nhà Xuất Bản Đà Nẵng）", "Đại Học Quốc Gia Hà Nội Trường Đại Học Ngoại Ngữ", "「Từ Điển Anh Việt」(Nhà Xuất Bản Giáo Dục)", "Nguyễn Sanh Phúc và Nhóm Cộng Tác(2000) 「Từ Điển Việt Anh」", "(Nhà Xuất Bản Thế Giới)"],
   vmod_ms1_url : { "" : "" },
 };
 
-sidebar_setting = {
-  sub_menu_snd : "",
-  sub_menu_talk : "",
-  sub_menu_gram : "",
-  sub_menu_vocab : "",
+db_info = {
+  user : "vi",
+  host : "localhost",
+  database : "vmod_vi",
+  password : "nuocmam",
 }
 
 exports.lang_info = lang_info
-exports.sidebar_setting = sidebar_setting
+exports.db_info = db_info
