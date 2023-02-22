1fb486060dcbbcad2f77d48f2587e12753820e6a
miho-miho <miho.021431.music@gmail.com>
2023-02-22 16:33:16 +0900

    Update app1.js

--- app1.js
+++ app1.js
@@ -266,7 +266,7 @@ app.get('/:lang/v/v_search', (req, res) => {
   })
   client.connect();
   const query = {
-    text: "SELECT T.index_char as index, t_word.index_char FROM t_index_char as T LEFT OUTER JOIN t_word ON T.index_char = t_word.index_char"
+    text: "SELECT T.index_char as index FROM t_index_char as T" //LEFT OUTER JOIN t_word ON T.index_char = t_word.index_char"
   };
   client.query(query, (err, result) => {
     console.log(result.rows);
