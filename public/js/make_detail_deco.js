cf173cd139e9767b895704565d1499520840a84d
miho-miho <miho.021431.music@gmail.com>
2023-02-22 15:08:30 +0900

    playDmodSound移行

--- public/js/make_detail_deco.js
+++ public/js/make_detail_deco.js
@@ -112,3 +112,19 @@ function replaceTokenColor(token, sentence, token_index){
       return sentence;
     }
   }
+  //playDmodSound
+  var dmodAudioObj;
+  function playDmodSound(dmodsound, startTime, stopTime){
+    if(dmodAudioObj != null)dmodAudioObj.pause();
+    dmodAudioObj = new Audio(dmodsound);
+    dmodAudioObj.currentTime = startTime;
+    dmodAudioObj.addEventListener("timeupdate", function (){
+      if (stopTime != 0 && dmodAudioObj != null && stopTime < dmodAudioObj.currentTime){
+        dmodAudioObj.pause();
+        dmodAudioObj.removeEventListener("timeupdate",this);
+        dmodAudioObj=null;
+            return;
+      }
+    });
+    dmodAudioObj.play();
+  }
