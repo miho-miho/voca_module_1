c49797bea3058c991cff841f4e2d5bb1553e7439
miho-miho <miho.021431.music@gmail.com>
2023-02-27 12:23:33 +0900

    Create config.js

---
+++ views/ar-sy/config.js
@@ -0,0 +1,20 @@
+//ar-sy：config.js
+lang_info = {
+  lang : "ar",
+  lang_jp : "シリアのアラビア語",
+  message1 : "",
+  message2 : "",
+  vmod_ms1 : ["池田修、竹田新（1981）『現代アラビア語小辞典』第三書館", "本田孝一、石黒忠昭（1997）『パスポート初級アラビア語辞典』白水社", "本田孝一、イハーブ・アハマド・イベード（2004）『パスポート日本語アラビア語辞典』白水社", "Cowan, J.M. ed.（1994）：The Hans Wehr Dictionary of Modern Written Arabic. Forth Edition, Spoken Language Service"],
+  vmod_ms2 :[ "المعجم الأساسي (1988) لاروس", "المنجد في اللغة العربية المعاصرة (2001) دار المشرق", "المراجعة اللغوية والتسجيل الصوتي : الأستاذ جمال زيتون"],
+  vmod_ms1_url : { "" : "" },
+};
+
+db_info = {
+  user : "as",
+  host : "localhost",
+  database : "vmod_as",
+  password : "supaisu",
+}
+
+exports.lang_info = lang_info
+exports.db_info = db_info
