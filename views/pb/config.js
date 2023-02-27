d221718d604fb7b87c463899907fabb13b255d0d
miho-miho <miho.021431.music@gmail.com>
2023-02-27 12:25:28 +0900

    追加

---
+++ views/pb/config.js
@@ -0,0 +1,20 @@
+//pb：config.js
+lang_info = {
+  lang : "Português",
+  lang_jp : "ブラジルポルトガル語",
+  message1 : "",
+  message2 : "",
+  vmod_ms1 : ["Portugues fundamental, metodos e documentos. tomo I e tomo II. 1987. INDC:Lisboa.","Mario Vilela (1991) Dicionario do Portugues Basico, Edicoes Asa: Lisboa."],
+  vmod_ms2 : [""],
+  vmod_ms1_url : { "" : "" },
+};
+
+db_info = {
+  user : "pb",
+  host : "localhost",
+  database : "vmod_pb",
+  password : "churrasco",
+}
+
+exports.lang_info = lang_info
+exports.db_info = db_info
